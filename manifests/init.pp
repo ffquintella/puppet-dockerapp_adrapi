5d9f0e0635a2732b03d798424e0a61caf6629a18
Felipe F Quintella <felipe@quintella.com>
2019-06-05 14:29:32 -0300

    INC

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,43 @@
 #
 # @example
 #   include dockerapp_adrapi
-class dockerapp_adrapi {
+class dockerapp_adrapi (
+  $service_name = 'adrapi',
+  $version = '0.4.7',
+  $ports = ['5001:5001'],
+){
+
+include 'dockerapp'
+
+  $image = "ffquintella/adrapi:${version}"
+
+  $dir_owner = 802
+  $dir_group = 802
+
+  $data_dir = $::dockerapp::params::data_dir
+  $config_dir = $::dockerapp::params::config_dir
+  $scripts_dir = $::dockerapp::params::scripts_dir
+  $lib_dir = $::dockerapp::params::lib_dir
+  $log_dir = $::dockerapp::params::log_dir
+
+  $conf_datadir = "${data_dir}/${service_name}"
+  $conf_configdir = "${config_dir}/${service_name}"
+  $conf_scriptsdir = "${scripts_dir}/${service_name}"
+  $conf_libdir = "${lib_dir}/${service_name}"
+  $conf_logdir = "${log_dir}/${service_name}"
+
+  $volumes = [
+  ]
+
+  $envs = []
+
+  dockerapp::run {$service_name:
+    image        => $image,
+    ports        => $ports,
+    volumes      => $volumes,
+    environments => $envs,
+    dir_group    => $dir_group,
+    dir_owner    => $dir_owner,
+  }
+
 }
--- spec/classes/dockerapp_adrapi_spec.rb
+++ spec/classes/dockerapp_adrapi_spec.rb
@@ -5,7 +5,20 @@ describe 'dockerapp_adrapi' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
+      let(:params) do
+        {
+          version: '1.4.1',
+          service_name: 'adrapi_test',
+        }
+      end
+
       it { is_expected.to compile }
+      it { is_expected.to contain_class('docker') }
+      it { is_expected.to contain_file('/srv/application-data/adrapi_test') }
+      it { is_expected.to contain_file('/srv/application-config/adrapi_test') }
+      it { is_expected.to contain_file('/srv/application-lib/adrapi_test') }
+      it { is_expected.to contain_file('/srv/application-log/adrapi_test') }
+      it { is_expected.to contain_file('/srv/scripts/adrapi_test') }
     end
   end
 end
